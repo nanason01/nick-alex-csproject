099eafd6bc65686f9f669557835f3acb9f72b81e
AlexDaines <adaines@umich.edu>
2021-10-10 22:31:10 -0400

    Alexs stock data and app

---
+++ app.py
@@ -0,0 +1,28 @@
+from stock_data import get_stock_data
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+df = get_stock_data('GME', '12/0``  11/20','05/01/21')
+fig = px.line(df, x= "t", y = "h")
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
